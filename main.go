8dee757ab9c4791c7890270f109ead013f921fbc
Neil Kuan <guan840912@gmail.com>
2021-08-09 16:34:35 +0800

    add get all vehicles route

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -17,6 +19,7 @@ func main() {
 	}
 	apiServer := gin.New()
 	apiServer.GET("/hello", HelloWorldHTTP)
+	apiServer.GET("/getallvehicles", GetAllVehicles)
 
 	apiServer.Run(":8080")
 
@@ -33,4 +36,33 @@ func HelloWorld(id string) {
 // HelloWorldHTTP is to Say Hello World to User.
 func HelloWorldHTTP(context *gin.Context) {
 	context.JSON(http.StatusOK, "Hello World")
+}
+
+// GetAllVehicles is to Say Hello World to User.
+func GetAllVehicles(context *gin.Context) {
+	url := "https://owner-api.teslamotors.com/api/1/vehicles"
+
+	// Create a Bearer string by appending string access token
+	var bearer = "Bearer " + os.Getenv("ACCESS_TOKEN")
+
+	// Create a new request using http
+	req, err := http.NewRequest("GET", url, nil)
+
+	// add authorization header to the req
+	req.Header.Add("Authorization", bearer)
+
+	// Send req using http Client
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error while reading the response bytes:", err)
+	}
+	log.Println(string([]byte(body)))
+	context.JSON(http.StatusOK, string([]byte(body)))
 }
